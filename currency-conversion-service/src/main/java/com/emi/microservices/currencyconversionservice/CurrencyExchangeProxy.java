475a2d8a95db6be0859b6cd142139f23ff7c4974
emily <wang.shig@northeastern.edu>
2023-07-04 16:42:57 -0400

    auto load balance with eureka

--- currency-conversion-service/src/main/java/com/emi/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/emi/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange",url="localhost:8000") //this name must be the application name of for the service we want to call
+// @FeignClient(name="currency-exchange",url="localhost:8000") //this name must be the application name of for the service we want to call
+/*the above is hardcoding the service instance we want to call in this service
+instead we can use feign to talk to eureka and auto load balance among the instances that can be used
+*/
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
     
     @GetMapping("/currency-exchange/from/{from}/to/{to}")

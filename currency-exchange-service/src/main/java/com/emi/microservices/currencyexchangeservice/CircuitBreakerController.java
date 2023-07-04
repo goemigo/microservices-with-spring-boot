8af476aeab9791dfb65a0b76daa9c9c32135a496
emily <wang.shig@northeastern.edu>
2023-07-04 18:22:25 -0400

    customized setting for circuit breaker

--- currency-exchange-service/src/main/java/com/emi/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/emi/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -14,7 +16,10 @@ public class CircuitBreakerController {
 
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
-    @Retry(name="sample-api",fallbackMethod = "hardcodedResponse") //default will retry three times if failure
+    // below @ are all for limiting requests
+    // @Retry(name="sample-api",fallbackMethod = "hardcodedResponse") //default will retry three times if failure
+    // @CircuitBreaker(name="default",fallbackMethod = "hardcodedResponse") 
+    @RateLimiter(name="default")
     public String sampleApi(){
         logger.info("Sample APi call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-url", String.class);

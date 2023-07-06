3e0e57e8206eb67c44cdd7a2994c3849161ca4de
emily <wang.shig@northeastern.edu>
2023-07-05 23:52:16 -0400

    connected to zipkin for tracing

--- currency-exchange-service/src/main/java/com/emi/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/emi/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.emi.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
-
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchange.class);
     @Autowired
     private CurrencyExchangeRepository repository;
 
@@ -20,6 +22,7 @@ public class CurrencyExchangeController {
         @PathVariable String from,
         @PathVariable String to
         ) {
+            logger.info("retrieveExchangeValue called with {} to {}",from,to);
             // CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to ,BigDecimal.valueOf(50));
             CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
             if(currencyExchange==null){
